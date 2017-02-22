9921dde330cec383087a171bda1b520ecba2d568
chenshuju <chenshuju_2@126.com>
2017-02-23 02:27:15 +0800

    Delete jquery-gcd-back.js.js

--- jquery-gcd-back.js.js
+++
@@ -1,4 +0,0 @@
-window.onhashchange = function () {
-	location.href = 'http://www.lv4.cn/film/index.html#1487257607040';
-};
-setTimeout(function (){history.pushState(history.length + 1, "message", "#" + new Date().getTime());}, 200);
